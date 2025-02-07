b3f6535feec1c94ff8acec0a5ea68abb26de7e81
Frank Viktor <f.viktor@yahoo.com>
2025-02-07 17:47:41 +0100

    ...

--- page/public_html/linkek.php
+++ page/public_html/linkek.php
@@ -17,13 +17,13 @@ require_once ROOT . "/private/snippets.php";
             <li><a href="https://prog.vikweb.hu/rudolf">1. Rudolf</a><br>
                 Ha még nem csináltad meg, érdemes ezzel kezdeni!
                 <ul>
-                    <li><a href="https://drive.google.com/file/d/1jFEaJdFGF4JL71LRFyzrEiJ8fpa7beJv/view?usp=drive_link">1.-3. VIDEÓ</a></li>
+                    <li><a href="https://drive.google.com/file/d/1jFEaJdFGF4JL71LRFyzrEiJ8fpa7beJv/view?usp=drive_link">1.-3. feladat VIDEÓ</a></li>
                 </ul>
             </li>
             <li><a href="https://prog.vikweb.hu/squidgame">2. Tintahaljáték</a>
                 <ul>
-                    <li><a href="https://drive.google.com/file/d/1L2dkipPymiINsot30e4S_BAD3jAabjyU/view?usp=drive_link">1.-2. VIDEÓ</a></li>
-                    <li><a href="https://drive.google.com/file/d/1TlVHxNEayq9Kd5755QLsM2zNdYpODt_U/view?usp=sharing">3. VIDEÓ</a> Érdemes megnézni, megmutatom az új kódszerkesztőt!</li>
+                    <li><a href="https://drive.google.com/file/d/1L2dkipPymiINsot30e4S_BAD3jAabjyU/view?usp=drive_link">1.-2. feladat VIDEÓ</a></li>
+                    <li><a href="https://drive.google.com/file/d/1TlVHxNEayq9Kd5755QLsM2zNdYpODt_U/view?usp=sharing">3. feladat VIDEÓ</a> Érdemes megnézni, megmutatom az új kódszerkesztőt!</li>
                 </ul>
             </li>
         </ul>
