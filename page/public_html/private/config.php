bcddb46d1a31f48288c2de75860a5bc89925f469
Frank Viktor <f.viktor@yahoo.com>
2025-02-26 17:23:04 +0100

    added internal encoding

--- page/public_html/private/config.php
+++ page/public_html/private/config.php
@@ -1,6 +1,7 @@
 <?php
 if ( !defined('ROOT') )
     define('ROOT', __DIR__ . "/..");
+mb_internal_encoding("UTF-8");
 require_once ROOT . "/local/config.php";
 require_once ROOT . "/private/error-reporting.php";
 setErrorReporting(Config::ERROR_REPORTING);
